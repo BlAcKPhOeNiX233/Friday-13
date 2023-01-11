7815671b45b107faff36cb5e1a55a95262705db1
BlAcKPhOeNiX233 <105356447+BlAcKPhOeNiX233@users.noreply.github.com>
2023-01-11 14:37:13 +0100

    Add files via upload

---
+++ Friday-13/Friday-13/Friday_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Friday_13App.swift
+//  Friday-13
+//
+//  Created by Bryan Sánchez Peralta on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Friday_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Friday-13/Friday-13UITests/Friday_13UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Friday_13UITestsLaunchTests.swift
+//  Friday-13UITests
+//
+//  Created by Bryan Sánchez Peralta on 11/01/23.
+//
+
+import XCTest
+
+final class Friday_13UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
