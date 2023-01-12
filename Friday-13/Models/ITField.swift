5b295a4ebe9296f9c1c690a78fd36821f7d5b77a
BlAcKPhOeNiX233 <bryansanchez.311@gmail.com>
2023-01-12 17:09:49 +0100

    Added to ITField the parameters: salary, demand and paths. Created Path structure

---
+++ Friday-13/Models/ITField.swift
@@ -0,0 +1,14 @@
+//
+//  ITFields.swift
+//  Friday-13
+//
+//  Created by Bryan Sánchez Peralta on 12/01/23.
+//
+
+import Foundation
+
+struct ITField {
+    let title: String, image: String, description: String, salary: String, demand: String;
+    let paths: [Path]
+    var percentage: Int?;
+}
---
+++ Friday-13/Models/Path.swift
@@ -0,0 +1,13 @@
+//
+//  Path.swift
+//  Friday-13
+//
+//  Created by Bryan Sánchez Peralta on 12/01/23.
+//
+
+import Foundation
+
+struct Path {
+    let difficulty: String;
+    let task: [String];
+}
