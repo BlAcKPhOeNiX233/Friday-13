d339e045cf1c82fca1940413e7fdf39a97c85b85
BlAcKPhOeNiX233 <bryansanchez.311@gmail.com>
2023-01-24 15:59:27 +0100

    Code refactoring

--- Friday-13/Views/MyITFieldsViews/MyITFieldsView.swift
+++ Friday-13/Views/MyITFieldsViews/MyITFieldsView.swift
@@ -19,78 +19,14 @@ struct MyITFieldsView: View {
                             .opacity(0.15)
                             .font(.headline)
                             .frame(width: 350, height: 240)
-                        TabView {
-                            ForEach(0 ..< itFieldViewModel.itFields.count, id: \.self) { position in
-                                if itFieldViewModel.itFields[position].isSelected == true {
-                                    NavigationLink(
-                                        destination: DescriptionView(
-                                            title: itFieldViewModel.itFields[position].title,
-                                            description: itFieldViewModel.itFields[position].description!,
-                                            salary: itFieldViewModel.itFields[position].salary!,
-                                            demand: itFieldViewModel.itFields[position].demand!,
-                                            paths: itFieldViewModel.itFields[position].paths!,
-                                            position: position,
-                                            isSelected: itFieldViewModel.itFields[position].isSelected,
-                                            change: change
-                                        )
-                                    ) {
-                                        ZStack {
-                                            Image(itFieldViewModel.itFields[position].image!)
-                                                .resizable()
-                                                .frame(width: 342, height: 235)
-                                                .foregroundColor(.cyan)
-                                                .cornerRadius(10)
-                                            ZStack {
-                                                Rectangle()
-                                                    .frame(width: 342, height: 50)
-                                                    .foregroundColor(Color.black.opacity(0.5))
-                                                Text(itFieldViewModel.itFields[position].title)
-                                                    .foregroundColor(Color.white)
-                                            }.padding(.top, 190)
-                                        }.cornerRadius(10)
-                                    }
-                                }
-                            }
-                        }.tabViewStyle(.page(indexDisplayMode: .never)).frame(height: 250)
+                        getFields(areSelected: true)
                     }
                     Text("Other IT Fields")
                         .fontWeight(.semibold)
                         .font(.title2)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 15)
-                    TabView {
-                        ForEach(0 ..< itFieldViewModel.itFields.count, id: \.self) { position in
-                            if itFieldViewModel.itFields[position].isSelected == false {
-                                NavigationLink(
-                                    destination: DescriptionView(
-                                        title: itFieldViewModel.itFields[position].title,
-                                        description: itFieldViewModel.itFields[position].description!,
-                                        salary: itFieldViewModel.itFields[position].salary!,
-                                        demand: itFieldViewModel.itFields[position].demand!,
-                                        paths: itFieldViewModel.itFields[position].paths!,
-                                        position: position,
-                                        isSelected: itFieldViewModel.itFields[position].isSelected,
-                                        change: change
-                                    )
-                                ) {
-                                    ZStack {
-                                        Image(itFieldViewModel.itFields[position].image!)
-                                            .resizable()
-                                            .frame(width: 342, height: 235)
-                                            .foregroundColor(.cyan)
-                                            .cornerRadius(10)
-                                        ZStack {
-                                            Rectangle()
-                                                .frame(width: 342, height: 50)
-                                                .foregroundColor(Color.black.opacity(0.5))
-                                            Text(itFieldViewModel.itFields[position].title)
-                                                .foregroundColor(Color.white)
-                                        }.padding(.top, 190)
-                                    }.cornerRadius(10)
-                                }
-                            }
-                        }
-                    }.tabViewStyle(.page(indexDisplayMode: .never)).frame(height: 250)
+                    getFields(areSelected: false)
                     Spacer()
                 }.navigationTitle("My IT Fields")
             }
@@ -98,6 +34,41 @@ struct MyITFieldsView: View {
     }
     func change(position: Int) {
         itFieldViewModel.itFields[position].isSelected.toggle()
+    }
+    func getFields(areSelected: Bool) -> some View {
+        return TabView {
+            ForEach(0 ..< itFieldViewModel.itFields.count, id: \.self) { position in
+                if itFieldViewModel.itFields[position].isSelected == areSelected {
+                    NavigationLink(
+                        destination: DescriptionView(
+                            title: itFieldViewModel.itFields[position].title,
+                            description: itFieldViewModel.itFields[position].description!,
+                            salary: itFieldViewModel.itFields[position].salary!,
+                            demand: itFieldViewModel.itFields[position].demand!,
+                            paths: itFieldViewModel.itFields[position].paths!,
+                            position: position,
+                            isSelected: itFieldViewModel.itFields[position].isSelected,
+                            change: change
+                        )
+                    ) {
+                        ZStack {
+                            Image(itFieldViewModel.itFields[position].image!)
+                                .resizable()
+                                .frame(width: 342, height: 235)
+                                .foregroundColor(.cyan)
+                                .cornerRadius(10)
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 342, height: 50)
+                                    .foregroundColor(Color.black.opacity(0.5))
+                                Text(itFieldViewModel.itFields[position].title)
+                                    .foregroundColor(Color.white)
+                            }.padding(.top, 190)
+                        }.cornerRadius(10)
+                    }
+                }
+            }
+        }.tabViewStyle(.page(indexDisplayMode: .never)).frame(height: 250)
     }
 }
 
